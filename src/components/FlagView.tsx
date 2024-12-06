5bf6d65956bba3ebcbb90eb181cb91207f1fd010
Kwaku Amofah-Boafo <boafo819@gmail.com>
2024-12-06 00:06:57 -0500

    Created FlagView component - component for showing flag of the day. (Note a default flag is set)

---
+++ src/components/FlagView.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import { useFlagData } from '../hooks/useFlagData';
+export const FlagView = () => {
+  // const {flagData} = useFlagData()
+  return (
+    <div className="flex items-center justify-center">
+      <div className="flex w-full items-center justify-center bg-[#222121] text-[250px]">
+        {'🇬🇭'}
+      </div>
+    </div>
+  );
+};
