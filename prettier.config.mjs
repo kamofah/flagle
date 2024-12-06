f1b5396c636984de842b71227c016b21592768ff
Kwaku Amofah-Boafo <boafo819@gmail.com>
2024-12-05 22:33:42 -0500

    Setting up configuration files

---
+++ prettier.config.mjs
@@ -0,0 +1,10 @@
+export default {
+    semi: true,
+    singleQuote: true,
+    trailingComma: 'es5',
+    tabWidth: 2,
+    printWidth: 60,
+    endOfLine: 'auto',
+    plugins: ['prettier-plugin-tailwindcss'],
+  };
+  
