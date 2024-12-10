4ad2ef4b565f30514e038c0f409a9ae87bd0d78c
Kwaku Amofah-Boafo <boafo819@gmail.com>
2024-12-09 23:21:55 -0500

    Renaming utils files to use TypeScript

--- src/utils/formatting.ts
+++ src/utils/formatting.ts
@@ -1,10 +1,6 @@
-const formatContinentText = (continents) => {
+export const formatContinentText = (continents) => {
   if (continents.length == 2) {
     return `${continents[0]}/${continents[1]}`;
   }
   return continents[0];
-};
-
-module.exports = {
-  formatContinentText,
 };
