b1a71a10f8b342fa271cc49dca613ec8be120efe
Kwaku Amofah-Boafo <boafo819@gmail.com>
2024-12-10 23:40:24 -0500

    Updated TextInput to utilized datalist elements # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # # On branch main # Your branch is up to date with 'origin/main'. # # Changes to be committed: #	modified:   src/components/ui/TextInput.tsx # # Changes not staged for commit: #	modified:   src/App.jsx #	modified:   src/components/EnabledAttempt.tsx #	modified:   src/components/Form.tsx #	modified:   src/components/PlayView.tsx #	modified:   src/index.css #	modified:   src/utils/misc.ts # # Untracked files: #	src/utils/validation.ts #

--- src/components/ui/TextInput.tsx
+++ src/components/ui/TextInput.tsx
@@ -1,18 +1,19 @@
 import React from 'react'
 
 type TextInputProps = {
-    isDisabled: boolean,
+    placeholder: string,
+    list?: string,
     onChangeAction: (e) => void,
-    enterAction: (e) => void
+    enterAction: (e) => void,
+    isDisabled: boolean,
 }
 
-
-const TextInput: React.FC<TextInputProps> = ({isDisabled, onChangeAction, enterAction}) => {
+const TextInput: React.FC<TextInputProps> = ({placeholder, list, onChangeAction, enterAction, isDisabled}) => {
   return (
     <input
-        className="w-full border border-solid border-white p-5 text-base text-white"
-        list="countryNames"
-        placeholder="Country/Territory"
+        className="w-ful p-5 text-base text-black focus: outline-none"
+        list={list}
+        placeholder={placeholder} 
         onChange={(event) => {onChangeAction(event.target.value);}}
         onKeyDown={(e) => {e.key === 'Enter' && enterAction(e);}}
         disabled={isDisabled}
